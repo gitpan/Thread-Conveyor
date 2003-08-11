e1f7b9b5e61e9d4cc2a4d3a6a8d5613da8e3cf65
Elizabeth Mattijsen <liz@dijkmat.nl>
2003-08-11 18:17:37 +0000

    Import of ELIZABETH/Thread-Conveyor-0.14 from CPAN.
    gitpan-cpan-distribution: Thread-Conveyor
    gitpan-cpan-version:      0.14
    gitpan-cpan-path:         ELIZABETH/Thread-Conveyor-0.14.tar.gz
    gitpan-cpan-author:       ELIZABETH
    gitpan-cpan-maturity:     released

--- lib/Thread/Conveyor/Throttled.pm
+++ lib/Thread/Conveyor/Throttled.pm
@@ -4,7 +4,7 @@ package Thread::Conveyor::Throttled;
 # Make sure we're a conveyor belt
 # Make sure we do everything by the book from now on
 
-$VERSION = '0.13';
+$VERSION = '0.14';
 @ISA = qw(Thread::Conveyor);
 use strict;
 
@@ -276,7 +276,7 @@ Please report bugs to <perlbugs@dijkmat.nl>.
 
 =head1 COPYRIGHT
 
-Copyright (c) 2002 Elizabeth Mattijsen <liz@dijkmat.nl>. All rights
+Copyright (c) 2002-2003 Elizabeth Mattijsen <liz@dijkmat.nl>. All rights
 reserved.  This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
 
